ff866e2a37009100462517452fb7764739646ed0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-04-16 06:41:10 +0200

    merge

--- bundles/org.eclipse.orion.client.javascript/web/javascript/validator.js
+++ bundles/org.eclipse.orion.client.javascript/web/javascript/validator.js
@@ -60,8 +60,7 @@ define([
 			'no-fallthrough' : 2,  //$NON-NLS-0$
 			'no-empty-block' : 0,  //$NON-NLS-0$
 			'valid-typeof': 2,  //$NON-NLS-0$
-			'no-sparse-arrays': 1,  //$NON-NLS-0$
-			'no-non-nls-literals': 0  //$NON-NLS-0$
+			'no-sparse-arrays': 1  //$NON-NLS-0$
 		},
 		
 		/**
